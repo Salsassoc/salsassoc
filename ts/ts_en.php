61c6fb1ae555a1664cae0d47ebcf2e7ba5edbd1a
Eric Beuque <eric.beuque@gmail.com>
2019-05-03 12:45:46 +0200

    Translate cotisations page

---
+++ ts/ts_en.php
@@ -0,0 +1,12 @@
+<?php
+class TS {
+	const Cotisation_CotisationCount = "Cotisation count: ";
+	const Cotisation_Label = "Label";
+	const Cotisation_StartDate = "Start date";
+	const Cotisation_EndDate = "End date";
+	const Cotisation_Amount = "Amount";
+	const Cotisation_Members = "Members";
+	const Cotisation_TotalAmount = "Total amount";
+	const Cotisation_View = "View";
+};
+?>
---
+++ ts/ts_fr.php
@@ -0,0 +1,12 @@
+<?php
+class TS {
+	const Cotisation_CotisationCount = "Nombre de cotisation : ";
+	const Cotisation_Label = "Libellé";
+	const Cotisation_StartDate = "Date début";
+	const Cotisation_EndDate = "Date de fin";
+	const Cotisation_Amount = "Montant";
+	const Cotisation_Members = "Membres";
+	const Cotisation_TotalAmount = "Montant total";
+	const Cotisation_View = "Voir";
+};
+?>
