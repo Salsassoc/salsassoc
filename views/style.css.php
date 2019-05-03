1152fe007bd6c0d9ba63f9b58c10d7c3301316db
Eric Beuque <eric.beuque@gmail.com>
2019-05-03 14:31:42 +0200

    Improve css

--- views/style.css.php
+++ views/style.css.php
@@ -38,14 +38,50 @@ body {
 	padding-right:10px;
 }
 
-.list th
-{
-    padding: 2px;
-    border-bottom: 1px solid black;
+a {
+	color:#22A1C4;
+	text-decoration:none;
 }
 
-.list td
+a:hover {
+	text-decoration:underline;
+}
+
+.list
+{
+	border-collapse: collapse;
+    border-left: 1px solid #D4D4D4;
+    border-right: 1px solid #D4D4D4;
+	font-size: 12px;
+}
+
+.list thead
+{
+	background-color:#D4D4D4;
+	color:#505050;
+	padding:0px;
+}
+
+.list th
+{
+    padding: 5px 2px 5px 2px;
+    border: 1px solid #D4D4D4;
+}
+
+.list tbody
+{
+	background-color:#F7F7F7;
+}
+
+.list tr:hover
+{
+	background-color:#E7E7E7;
+}
+
+.list td 
 {
     padding: 2px;
     text-align: center;
+    border-bottom: 1px solid #D4D4D4;
+    border-top: 1px solid #D4D4D4;
 }
